7eb8a7bfda464ff0d70b1a2e56751c9e8a0a035e
cppdev <fckngRaccoon@yandex.ru>
2023-03-20 00:44:58 +0300

    Ex02

---
+++ Ex02/Program.cs
@@ -0,0 +1,57 @@
+﻿const int COEFFICIENT = 0;
+const int CONSTANT = 1;
+const int X_COORD = 0;
+const int Y_COORD = 1;
+const int LINE1 = 1;
+const int LINE2 = 2;
+
+double[] lineData1 = InputLineData(LINE1);
+double[] lineData2 = InputLineData(LINE2);
+
+if (ValidateLines(lineData1, lineData2))
+{
+    double[] coord = FindCoords(lineData1, lineData2);
+    Console.Write($"Координаты: ({coord[X_COORD]},{coord[Y_COORD]})");
+}
+
+double Prompt(string message)
+{
+    System.Console.Write(message);
+    string value = Console.ReadLine();
+    double result = Convert.ToDouble(value);
+    return result;
+}
+
+double[] InputLineData(int numberOfLine)
+{
+    double[] lineData = new double[2];
+    lineData[COEFFICIENT] = Prompt($"Коэф: ");
+    lineData[CONSTANT] = Prompt($"Конст: ");
+    return lineData;
+}
+
+double[] FindCoords(double[] lineData1, double[] lineData2)
+{
+    double[] coord = new double[2];
+    coord[X_COORD] = (lineData1[CONSTANT] - lineData2[CONSTANT]) / (lineData2[COEFFICIENT] - lineData1[CONSTANT]);
+    coord[Y_COORD] = lineData1[CONSTANT] * coord[X_COORD] + lineData1[CONSTANT];
+    return coord;
+}
+
+bool ValidateLines(double[] lineData1, double[] lineData2)
+{
+    if(lineData1[COEFFICIENT] == lineData2[COEFFICIENT])
+    {
+        if(lineData1[CONSTANT] == lineData2[CONSTANT])
+        {
+            Console.WriteLine("Совпадают");
+            return false;
+        }
+        else
+        {
+            Console.WriteLine("Параллельны")
+            return false;
+        }
+    }
+    return true;
+}
