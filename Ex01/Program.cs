c04c29590d0357981550350f2049d20ebcb0d5d8
cppdev <fckngRaccoon@yandex.ru>
2023-03-19 23:58:09 +0300

    Ex01

---
+++ Ex01/Program.cs
@@ -0,0 +1,44 @@
+﻿int Prompt(string message)
+{
+    System.Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+int[] InputArray(int lenght)
+{
+    int[] array = new int[lenght];
+    for (int i = 0; i < array.Length; i++);
+    {
+        array[i] = Prompt($"Введите {i + 1} элемент: ");
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"a[{i}] = {array[i]}");
+    }
+}
+
+int CountPositiveNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int lenght = Prompt("Кол-во эл-тов: ");
+int[] array;
+array = InputArray(lenght);
+PrintArray(array);
+Console.WriteLine(CountPositiveNumbers(array));
